02178272b522cd8541cc796758b427c90a210767
One-888 <53633937+One-888@users.noreply.github.com>
2022-04-05 14:25:25 -0400

    Update Dup-Finder.ps1

--- Dup-Finder.ps1
+++ Dup-Finder.ps1
@@ -1,19 +1,24 @@
-$src = @("C:\All Photos")  # Can check multiple folders seperate by comma
-$dup = "C:\Dup" # Export File if you want it to export 
+$i = 0
+Write-Progress -Activity "Caluating.." -PercentComplete 5
 
-$ftype = "*.*" 
-# Check file Dup by checking the file size - Setp 2
-$a = gci -Path $src -Filter $ftype -Recurse -File -ErrorAction SilentlyContinue  | `
-Where-Object { $_.Length -gt 1mb} | Group-Object -Property Length | `
-Where-Object { $_.Count -gt 1 } |  Select -ExpandProperty Group |  % {$_.FullName}  
+$src = @("C:\Users") #"F:\All Photos"
+$dup = "C:\Users\VSayakanit\OneDrive - Pittsburgh Water and Sewer Authority\Dup"
 
-# Export Directory
-mkdir $dup -ErrorAction SilentlyContinue
+$ftype = "*.zip" 
 
-# Using Has Funxtion to check duplicate file - Step 2 
-# Show result in csv file
-$a | %{Get-filehash $_.ToString() -Algorithm SHA1}  | `
+$a = gci -Path $src -Filter $ftype -Recurse  -File  -ErrorAction SilentlyContinue | `
+Where-Object { $_.Length -gt 1kb} | Group-Object -Property Length | `
+Where-Object { $_.Count -gt 1 } |  Select -ExpandProperty Group | `
+ % {$_.FullName}  # Check file Dup by checking the file size - Setp 1
+
+mkdir $dup -ErrorAction SilentlyContinue # Export Directory
+$stat = $a.Count
+
+$a | %{                                   `
+ $i = $i + 1; $pct = [math]::Round(($i / $stat)*100); `
+Write-Progress -Activity "Caluating.." -PercentComplete $pct;   `
+Get-filehash $_.ToString() -Algorithm SHA1}  | `
 Group-Object -Property Hash | `
-Where-Object { $_.Count -gt 1 } | `
-Select -ExpandProperty group | select hash,@{name="Move";expression={"Move-Item -Destination ""$dup"" """ + $_.path+""""}} | `
-Export-Csv -Path ($dup + "\Dup_Result.csv") -NoTypeInformation
+Where-Object { $_.Count -gt 1 -and $_.Group -like "@{*" } | `
+Select -ExpandProperty group | select hash,@{name="File";expression={split-path  $_.path -leaf}},@{name="Move";expression={"Move-Item -Destination ""$dup"" """ + $_.path+""""}} | `
+Export-Csv -Path ($dup + "\Dup.csv") -NoTypeInformation # Using Hash Function to check duplicate file - Step 2 
