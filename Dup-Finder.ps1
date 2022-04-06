896e620a4f8f73bcd3b7547f2455b13f88f90e44
One-888 <53633937+One-888@users.noreply.github.com>
2022-04-06 11:38:38 -0400

    Update Dup-Finder.ps1
    Add Report Section

--- Dup-Finder.ps1
+++ Dup-Finder.ps1
@@ -20,5 +20,17 @@ Write-Progress -Activity "Caluating.." -PercentComplete $pct;   `
 Get-filehash $_.ToString() -Algorithm SHA1}  | `
 Group-Object -Property Hash | `
 Where-Object { $_.Count -gt 1 -and $_.Group -like "@{*" } | `
-Select -ExpandProperty group | select hash,@{name="File";expression={split-path  $_.path -leaf}},@{name="Move";expression={"Move-Item -Destination ""$dup"" """ + $_.path+""""}} | `
-Export-Csv -Path ($dup + "\Dup.csv") -NoTypeInformation # Using Hash Function to check duplicate file - Step 2 
+Select -ExpandProperty group | select hash,@{name="File";expression={split-path  $_.path -leaf}},@{name="Move";expression={"Move-Item -Destination ""$dup"" """ + $_.path+""""}}
+
+$export_text | select hash,File | Format-Table -AutoSize -Wrap
+#$export_text | Export-Csv -Path ($dup + "\Dup.csv") -NoTypeInformation # Using Hash Function to check duplicate file - Step 2 
+
+$move_text = $export_text | Group-Object -property {$_.hash } | Where-Object { $_.Count -gt 1}  | ` 
+% { """" + $_.Group[0].Path + """" } # | Format-Table -AutoSize - # $_.Group[0].Hash + ", """ + $_.Group[0].File+ """, """ + $_.Group[0].Path+ """"
+
+"`nInvoke-Item Section"
+$export_text | % {"ii """ + (split-Path -Path $_.path).ToString() + """" }
+
+"`nMove-Item Section"
+$move_text | % {"mv -Destination ""$dup"" """ + $_ + """" }
+
